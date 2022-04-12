b7a7a97aba0f1efbdf7d07399e07d79b05f2f1c4
masaya0521 <kaji0521km@gmail.com>
2022-04-13 00:17:18 +0900

    add json

--- backend/server.go
+++ backend/server.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Post struct {
+	User    string
+	Thereds []string
+}
 
 func handler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "HelloWorld,%s!", request.URL.Path[1:])
@@ -20,9 +26,20 @@ func redirectHandler(w http.ResponseWriter, request *http.Request) {
 	w.WriteHeader(302)
 }
 
+func jsonHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	post := &Post{
+		User:    "user",
+		Thereds: []string{"1", "2", "3"},
+	}
+	json, _ := json.Marshal(post)
+	w.Write(json)
+}
+
 func main() {
 	http.HandleFunc("/v1/hello", handler)
 	http.HandleFunc("/write", writeHandler)
 	http.HandleFunc("/redirect", redirectHandler)
+	http.HandleFunc("/json", jsonHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
