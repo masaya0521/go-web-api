36df843a4f9d999242e6b83b78608c3067293f92
masaya0521 <kaji0521km@gmail.com>
2022-04-12 23:14:59 +0900

    add 501 error

--- backend/server.go
+++ backend/server.go
@@ -10,7 +10,13 @@ func handler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "HelloWorld,%s!", request.URL.Path[1:])
 }
 
+func writeHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(501)
+	fmt.Fprintf(w, "HelloWorld!")
+}
+
 func main() {
 	http.HandleFunc("/v1/hello", handler)
+	http.HandleFunc("/write", writeHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
