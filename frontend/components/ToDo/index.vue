e7626ed254a0235a700b1744042a4513a500d48e
masaya0521 <kaji0521km@gmail.com>
2022-06-19 00:07:07 +0900

    InputBoxの追加

--- frontend/components/ToDo/index.vue
+++ frontend/components/ToDo/index.vue
@@ -1,24 +1,19 @@
 <template>
-  <div class="justify-center">
-    <button class="shadow h-10 w-48">test</button>
-    <input type="text" class="shadow h-10 w-48" />
+  <div class="container mx-auto">
+    <div class="flex flex-col">
+      <span class="text-gray-800 text-4xl my-16">ToDo List</span>
+      <InputBox />
+      <button class="shadow text-gray-800 h-10 w-48 px-4 py-2 m-2">test</button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import InputBox from '~/components/Todo/InputBox/InputBox.vue';
 
 export default Vue.extend({
   name: 'ToDo',
-  data() {
-    return {
-      message: 'This is a message',
-      api: [],
-      post: {
-        content: 'hello world',
-        author: 'masaya',
-      },
-    };
-  },
+  components: { InputBox },
 });
 </script>
